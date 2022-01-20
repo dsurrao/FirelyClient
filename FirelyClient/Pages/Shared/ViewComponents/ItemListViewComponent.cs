19bb13688df8d45a4816cb379ad4af126e65bc75
Dominic Surrao <dsurrao@hotmail.com>
2022-01-20 17:36:34 -0500

    added sample view component

---
+++ FirelyClient/Pages/Shared/ViewComponents/ItemListViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirelyClient.Pages.Shared.ViewComponents
+{
+    public class ItemListViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var items = new List<string>() { "one", "two", "three" };
+            return View(items);
+        }
+    }
+}
